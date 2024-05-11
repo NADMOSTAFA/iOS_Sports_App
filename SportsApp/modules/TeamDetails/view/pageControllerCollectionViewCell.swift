2198e27995432494c454e8c41cf2bd8a9e79e2d9
Israa <isramuhammed613@gmail.com>
2024-05-11 07:58:38 -0700

    finish TeamDetails Screen Design

---
+++ SportsApp/modules/TeamDetails/view/pageControllerCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  pageControllerCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by Israa on 11/05/2024.
+//
+
+import UIKit
+
+class pageControllerCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var pageCount: UIPageControl!
+}
