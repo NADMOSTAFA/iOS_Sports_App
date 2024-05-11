791717ca180444b021525c5ebc4224de02147d7d
Israa <isramuhammed613@gmail.com>
2024-05-11 01:13:54 -0700

    finish league details Screen Design

---
+++ SportsApp/modules/leagueDetails/view/TeamCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TeamCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by Israa on 11/05/2024.
+//
+
+import UIKit
+
+class TeamCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var teamName: UILabel!
+    
+    @IBOutlet weak var teamLogo: UIImageView!
+}
