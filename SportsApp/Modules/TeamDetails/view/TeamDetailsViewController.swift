566a25e6649629bdb7bb5047f02b64e133816b69
Israa <isramuhammad613@gmail.com>
2024-05-15 14:07:59 -0700

    test network services  fetch data function

--- SportsApp/Modules/TeamDetails/view/TeamDetailsViewController.swift
+++ SportsApp/Modules/TeamDetails/view/TeamDetailsViewController.swift
@@ -7,26 +7,40 @@
 
 import UIKit
 
-class TeamDetailsViewController: UIViewController {
+class TeamDetailsViewController: UIViewController , UICollectionViewDelegate , UICollectionViewDataSource {
 
     @IBOutlet weak var teamName: UILabel!
     @IBOutlet weak var teamCoachName: UILabel!
     @IBOutlet weak var teamLogo: UIImageView!
+    
+    var viewModel : TeamDetailsViewModel?
+    var leagueDetailsViewModel : LeagueDetailsViewModel?
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        viewModel = TeamDetailsViewModel(network: NetworkService.instance, teamId: leagueDetailsViewModel?.getselectedTeam().teamKey)
+        
+        
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       
+        return viewModel?.getPlayersCount() ?? 0
+       
     }
-    */
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath) as! PlayerCollectionViewCell
+
+        return cell
+        
+    }
+    
 
 }
---
+++ SportsAppTests/NetworkServiceTest.swift
@@ -0,0 +1,68 @@
+//
+//  NetworkServiceTest.swift
+//  SportsAppTests
+//
+//  Created by Israa on 15/05/2024.
+//
+
+import XCTest
+@testable import SportsApp
+final class NetworkServiceTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testFetchData() {
+        // Given
+        let networkService = NetworkService.instance
+        let endPoint = "/football/"
+        let expectation = XCTestExpectation(description: "Fetch data expectation")
+        
+        // When
+        networkService.fetchData(from: endPoint, parameters: ["met":"Leagues"]) { (result: Result<League, Error>) in
+            // Then
+            switch result {
+            case .success(let response):
+                
+                XCTAssertNotNil(response, "Response should not be nil")
+                expectation.fulfill()
+                
+            case .failure(let error):
+                
+                XCTFail("Error occurred: \(error.localizedDescription)")
+            }
+        }
+        
+        wait(for: [expectation], timeout: 30)
+    }
+    
+    func testFetchDataFailure() {
+        // Given
+        let networkService = NetworkService.instance
+        let endPoint = "/nonexistent/"
+        let expectation = XCTestExpectation(description: "Fetch data failure expectation")
+        
+        // When
+        networkService.fetchData(from: endPoint, parameters: nil) { (result: Result<League, Error>) in
+            // Then
+            switch result {
+            case .success(let response):
+                XCTFail("Test case passed unexpectedly. Response: \(response)")
+            case .failure(let error):
+                XCTAssertNotNil(error, "Error should not be nil")
+                expectation.fulfill()
+            }
+        }
+        
+        wait(for: [expectation], timeout: 10)
+    }
+
+    }
+
+
+
